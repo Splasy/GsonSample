77e4a62b2a6da5585a8a089117df741c9bf26c9e
Aitor RM <aitorromojaromartinez@gmail.com>
2019-09-20 10:43:50 +0100

    cambio

--- src/main/java/gson/Persona.java
+++ src/main/java/gson/Persona.java
@@ -6,6 +6,24 @@ public class Persona {
 		
 		String nombre, apellidos;
 		int edad;
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		public String getApellidos() {
+			return apellidos;
+		}
+		public void setApellidos(String apellidos) {
+			this.apellidos = apellidos;
+		}
+		public int getEdad() {
+			return edad;
+		}
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
 
 	
 
